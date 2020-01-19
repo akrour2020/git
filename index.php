e51a0da3a17d88798b7b5d0cf1d1ca9063bcee42
tarek <hadil20131@gmail.com>
2020-01-19 10:18:34 +0100

    add style

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, ipsa fugiat. Ratione magnam cum, reiciendis molestias repellat ducimus, perspiciatis ullam quasi tenetur deserunt, possimus veritatis earum error nisi provident commodi.</p>
 
 <div>
-<h2>secod commit</h2>
+    <h2>secod commit</h2>
 </div>
+<footer>
+    <main>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, et alias assumenda, sequi culpa neque nam perferendis esse incidunt animi placeat omnis quae inventore molestiae error voluptate magnam unde. Velit!</main>
+</footer>
