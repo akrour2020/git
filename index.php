50d235d4f2ed32ae55edd0e3aa90621f342180e0
tarek <hadil20131@gmail.com>
2020-01-19 10:13:03 +0100

    add div to index

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <h1>Salam</h1>
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, ipsa fugiat. Ratione magnam cum, reiciendis molestias repellat ducimus, perspiciatis ullam quasi tenetur deserunt, possimus veritatis earum error nisi provident commodi.</p>
+
+<div>
+<h2>secod commit</h2>
+</div>
