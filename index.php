50c53cc517464d32436d186f32d5e5191801547a
tarek <hadil20131@gmail.com>
2020-01-19 10:04:22 +0100

    add paragraph

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <h1>Salam</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, ipsa fugiat. Ratione magnam cum, reiciendis molestias repellat ducimus, perspiciatis ullam quasi tenetur deserunt, possimus veritatis earum error nisi provident commodi.</p>
